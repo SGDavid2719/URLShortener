e0c356807514a2202b7452d5d13e4c9d8ea3f679
David SG <santome.galvan.david@gmail.com>
2024-06-26 23:46:44 +0200

    [Feature] Project properties

--- back/portfolio/src/test/java/com/urlshortener/UrlShortenerApplicationTests.java
+++ back/portfolio/src/test/java/com/urlshortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,7 @@
-package com.urlshortener.portfolio;
+package com.urlshortener;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
